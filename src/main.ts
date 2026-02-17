1c43503f6cbef838a32d0c09da22c05008eb213a
kenneth <joshuabecaro@gmail.com>
2026-02-17 12:24:57 +0800

    feat: add home content

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
-console.log("bruh");
+async function generateQuote() {
+  const quoteContainer = document.getElementById("quote-container");
+
+  try {
+    const response = await fetch(
+      "https://zen-quotes.kennethbecaro0.workers.dev/",
+    );
+
+    const quotes = await response.json();
+
+    if (quotes && quotes.length > 0) {
+      const quote = quotes[0];
+      quoteContainer!.innerHTML = `
+          <blockquote id="quote">
+              &ldquo;${quote.q}&rdquo; &mdash; <footer>${quote.a}</footer>
+          </blockquote>
+      `;
+    }
+  } catch (error) {
+    console.error("Error fetching quotes:", error);
+  }
+}
+
+await generateQuote();
